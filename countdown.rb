32208f408c2dd833f3c29159df9c044f6292fb23
Harold Torres Marino <haroldtm55@gmail.com>
2021-08-11 21:32:52 +1000

    Lab completed

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,17 @@
 #write your code here
 
-def countdown
+def countdown(n)
+  while n>=1
+    puts "#{n} SECOND(S)!"
+    n-=1
+  end
+  "HAPPY NEW YEAR!"
+end
+
+def countdown_with_sleep(n)
+  
+  sleep(n) && 
+  
+  'HAPPY NEW YEAR!'
+  
 end
